f27a0de80d55a02f947aa3651937556ee95a2c47
Noemi Baglieri <105011724+noemibaglieri@users.noreply.github.com>
2025-08-30 16:10:03 +0200

    fix

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import RegisterPage from "./components/RegisterPage";
 import LoginPage from "./components/LoginPage";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+       <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
